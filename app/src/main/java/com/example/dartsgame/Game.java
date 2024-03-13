bd6d85da03afc2f4865d10f88c7c36f6da4871a4
SOBREMESA\Alejandro <alejandro.ramon.palarea@gmail.com>
2024-03-13 18:17:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dartsgame/Game.java
@@ -0,0 +1,66 @@
+package com.example.dartsgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    GameLoop gameLoop;
+    Context context;
+    public Game(Context context) {
+        super(context);
+
+        // Get surface holder and callback
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        this.context = context;
+        gameLoop = new GameLoop();
+
+        // View focusable for capture events
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String avgNumber = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.yellow);
+        paint.setColor(color);
+        canvas.drawText("UPS: " + avgNumber, 100, 20, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String avgNumber = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.yellow);
+        paint.setColor(color);
+        canvas.drawText("FPS: " + avgNumber, 100, 40, paint);
+    }
+}
---
+++ app/src/main/java/com/example/dartsgame/GameLoop.java
@@ -0,0 +1,11 @@
+package com.example.dartsgame;
+
+public class GameLoop {
+    public double getAverageUPS() {
+        return 0;
+    }
+
+    public double getAverageFPS() {
+        return 0;
+    }
+}
